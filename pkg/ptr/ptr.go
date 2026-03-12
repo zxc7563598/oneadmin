9090bd401a535a8facefb05f7f4f7d7995a4d105
zxc7563598 <junjie.he.925@gmail.com>
2026-03-12 16:01:39 +0800

    feat(pkg): 增加指针转换函数

---
+++ pkg/ptr/ptr.go
@@ -0,0 +1,21 @@
+package ptr
+
+// Uint64 将 uint64 转换为 *uint64
+func Uint64(v uint64) *uint64 {
+	return &v
+}
+
+// Uint64 将 int 转换为 *int
+func Int(v int) *int {
+	return &v
+}
+
+// Uint64 将 string 转换为 *string
+func String(v string) *string {
+	return &v
+}
+
+// Uint64 将 bool 转换为 *bool
+func Bool(v bool) *bool {
+	return &v
+}
