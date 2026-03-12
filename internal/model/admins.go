64c3275528ad9018e149939a349752e659b68291
zxc7563598 <junjie.he.925@gmail.com>
2026-03-12 16:06:16 +0800

    refactor(model): 更改密码生成方式，去除 Salt

--- internal/model/admins.go
+++ internal/model/admins.go
@@ -7,7 +7,6 @@ type Admin struct {
 	Nickname string      `gorm:"type:varchar(100);not null;comment:昵称"`
 	Username string      `gorm:"type:varchar(100);not null;comment:用户名"`
 	Password string      `gorm:"type:varchar(255);not null;comment:密码"`
-	Salt     int         `gorm:"type:int;not null;comment:扰乱码"`
 	Token    *string     `gorm:"type:varchar(255);comment:登录凭证"`
 	RoleID   *uint64     `gorm:"comment:当前角色ID"`
 	Avatar   string      `gorm:"type:varchar(255);not null;default:'avatar.png';comment:头像"`
