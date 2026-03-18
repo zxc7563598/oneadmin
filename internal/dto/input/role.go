723b26c7800742fd850c297b94a3bf87543d8932
zxc7563598 <junjie.he.925@gmail.com>
2026-03-18 18:12:56 +0800

    feat(dto): 完善 role 模块 dto

---
+++ internal/dto/input/role.go
@@ -0,0 +1,23 @@
+package input
+
+// RoleListPageReq 获取角色分页列表请求
+type RoleListPageReq struct {
+	Page     int     `json:"page" binding:"required" err:"required=10201"`
+	PageSize int     `json:"page_size" binding:"required" err:"required=10201"`
+	Name     *string `json:"name"`
+	Enable   *int    `json:"enable"`
+}
+
+// RoleSaveReq 处理创建或变更角色请求
+type RoleSaveReq struct {
+	ID      *uint64  `json:"id"`
+	Code    string   `json:"code" binding:"required" err:"required=10202"`
+	Name    string   `json:"name" binding:"required" err:"required=10203"`
+	Enable  bool     `json:"enable" binding:"required" err:"required=10204"`
+	MenuIDs []uint64 `json:"menu_ids`
+}
+
+// RoleDeleteReq 删除角色请求
+type RoleDeleteReq struct {
+	RoleID uint64 `json:"id" binding:"required" err:"required=10205"`
+}
---
+++ internal/dto/resp/role.go
@@ -0,0 +1,44 @@
+package resp
+
+// RoleListPageResp 获取管理员分页列表返回
+type RoleListPageResp struct {
+	Total    int64 `json:"total"`
+	PageData []RoleListPageItem
+}
+
+type RoleListPageItem struct {
+	ID     uint64 `json:"id"`
+	Code   string `json:"code"`
+	Name   string `json:"name"`
+	Enable bool   `json:"enable"`
+}
+
+// RoleListAllResp 获取全部角色请求返回
+type RoleListAllResp struct {
+	List []RoleListPageItem `json:"list"`
+}
+
+// RolePermissionsResp 获取管理员菜单权限树请求返回
+type RolePermissionsResp struct {
+	Menu []RoleMenuItem `json:"menu"`
+}
+
+type RoleMenuItem struct {
+	ID          uint64         `json:"id"`
+	Code        string         `json:"code"`
+	Enable      bool           `json:"enable"`
+	Show        bool           `json:"show"`
+	KeepAlive   bool           `json:"keep_alive"`
+	Layout      string         `json:"layout"`
+	Type        string         `json:"type"`
+	ParentID    uint64         `json:"parent_id"`
+	Name        string         `json:"name"`
+	Icon        string         `json:"icon"`
+	Path        string         `json:"path"`
+	Component   string         `json:"component"`
+	Order       int            `json:"order"`
+	Redirect    string         `json:"redirect"`
+	Method      string         `json:"method"`
+	Description string         `json:"description"`
+	Children    []RoleMenuItem `json:"children"`
+}
