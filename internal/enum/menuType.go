fc9f89abff01fe1718bea575590ee40ed1233c0f
zxc7563598 <junjie.he.925@gmail.com>
2026-03-19 12:00:51 +0800

    feat(enum): 增加 menu 表 type 字段枚举信息

---
+++ internal/enum/menuType.go
@@ -0,0 +1,25 @@
+package enum
+
+import "github.com/zxc7563598/oneadmin/internal/i18n"
+
+type MenuType string
+
+const (
+	MenuTypeButton MenuType = "BUTTON"
+	MenuTypeMenu   MenuType = "MENU"
+)
+
+func (m MenuType) Key() string {
+	switch m {
+	case MenuTypeButton:
+		return "menu_type.button"
+	case MenuTypeMenu:
+		return "menu_type.menu"
+	default:
+		return "unknown"
+	}
+}
+
+func (m MenuType) Text(lang string) string {
+	return i18n.T(lang, m.Key())
+}
