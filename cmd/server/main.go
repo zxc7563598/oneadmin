f7d7a6c031b5c62c3b883ecaa554a1c95c475648
zxc7563598 <junjie.he.925@gmail.com>
2026-03-26 12:07:02 +0800

    refactor(app): 重构应用初始化逻辑，简化参数并添加Redis和数据库初始化

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,9 +14,6 @@ import (
 	"github.com/zxc7563598/oneadmin/docs"
 	"github.com/zxc7563598/oneadmin/internal/bootstrap"
 	"github.com/zxc7563598/oneadmin/internal/config"
-	"github.com/zxc7563598/oneadmin/internal/logger"
-	"github.com/zxc7563598/oneadmin/internal/migrate"
-	"github.com/zxc7563598/oneadmin/pkg/jwt"
 )
 
 // @title OneAdmin API
@@ -40,34 +37,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("无法加载配置: %v", err)
 	}
-	// 初始化日志
-	logger.InitAll()
-	// 初始化redis
-	rdb, err := config.InitRedis(cfg)
-	if err != nil {
-		log.Fatalf("Redis配置存在但连接失败: %v", err)
-	}
-	if rdb != nil {
-		defer rdb.Close()
-		log.Println("Redis已启用")
-	}
-	// 初始化数据库
-	db, err := config.InitDB(cfg)
-	if err != nil {
-		log.Fatalf("无法初始化数据库: %v", err)
-	}
-	// 自动建表
-	if err := migrate.Run(db); err != nil {
-		panic(err)
-	}
-	// 初始化填充数据
-	if err := migrate.Seed(db); err != nil {
-		panic(err)
-	}
-	// 初始化jwt
-	jwt.Init(cfg.JWT)
 	// 初始化 Gin 应用
-	r := bootstrap.NewApp(db, rdb)
+	r := bootstrap.NewApp(cfg)
 	// 创建 HTTP Server
 	addr := fmt.Sprintf(":%d", *port)
 	srv := &http.Server{
--- internal/bootstrap/app.go
+++ internal/bootstrap/app.go
@@ -1,13 +1,44 @@
 package bootstrap
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
+	"github.com/zxc7563598/oneadmin/internal/config"
 	"github.com/zxc7563598/oneadmin/internal/i18n"
-	"gorm.io/gorm"
+	"github.com/zxc7563598/oneadmin/internal/logger"
+	"github.com/zxc7563598/oneadmin/internal/migrate"
+	"github.com/zxc7563598/oneadmin/pkg/jwt"
 )
 
-func NewApp(db *gorm.DB, rdb *redis.Client) *gin.Engine {
+func NewApp(cfg *config.Config) *gin.Engine {
+	// 初始化日志
+	logger.InitAll()
+	// 初始化redis
+	rdb, err := config.InitRedis(cfg)
+	if err != nil {
+		log.Fatalf("Redis配置存在但连接失败: %v", err)
+	}
+	if rdb != nil {
+		defer rdb.Close()
+		log.Println("Redis已启用")
+	}
+	// 初始化数据库
+	db, err := config.InitDB(cfg)
+	if err != nil {
+		log.Fatalf("无法初始化数据库: %v", err)
+	}
+	// 自动建表
+	if err := migrate.Run(db); err != nil {
+		panic(err)
+	}
+	// 初始化填充数据
+	if err := migrate.Seed(db); err != nil {
+		panic(err)
+	}
+	// 初始化jwt
+	jwt.Init(cfg.JWT)
+	// 处理依赖注入
 	// repository
 	repos := InitRepositories(db)
 	// service
